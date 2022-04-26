e52cdadcae56a15c6f34e9448f16f60ff8253dce
Rudolf Zachar <rudolf.zachar@reachplc.com>
2022-04-26 13:28:42 +0100

    table layout+teamName val(needs check first team)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const leagueTable = {
         rows: []
     }
-
+    // Round of matches
     const matchRound = [];
 
     
@@ -19,11 +19,33 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
         const myForm = document.getElementById('team-form');
         let teamValue = myForm.team.value;
-        tableRow.teamName = teamValue;
+        // Before teamName is assigned to tableRow object, validate
+        const validatedTeamName = teamValue.trim()
 
+        // for( const teamObject of leagueTable.rows){
+        //   if(teamObject.teamName == validatedTeamName){
+        //     alert('Sorry, this Team Name already exists! Enter a unique name!')
+        //     break;
+        //   }
+        // }
+        document.getElementById('team').value = "";
+        
         //data row in table
         const tr = document.createElement('tr');
         const dTeamName = document.createElement('td');
+        
+        const tableTeamNames = document.querySelectorAll('.table-team-name')
+        if(tableTeamNames) {
+          for(const team of tableTeamNames) {
+            if(team.textContent == validatedTeamName) {
+              alert('Sorry, this Team Name already exists! Enter a unique name!')
+              return;
+            } else {
+              // orig. in after for loop with alert()
+              tableRow.teamName = validatedTeamName;
+            }
+          }
+        }
         tr.appendChild(dTeamName);
         dTeamName.textContent = tableRow.teamName;
         dTeamName.classList.add('table-team-name');
@@ -51,8 +73,12 @@ document.addEventListener('DOMContentLoaded', ()=> {
        
 
         const table = document.getElementById('table');
-        table.appendChild(tr);
+        const tbody = document.querySelector('tbody')
+        tbody.appendChild(tr)
+        // table.appendChild(tr);
         table.style.border = "solid 0.1rem black";
+        table.classList.remove("hidden")
+        table.classList.add("visible")
         document.getElementById('team').value = "";
 
         tableRow.id = Math.random()
@@ -66,26 +92,45 @@ document.addEventListener('DOMContentLoaded', ()=> {
     document.getElementById('team-submit').onclick = (e) => {
         e.preventDefault();
         getTeamWithStatsRowInit();
-        console.log(leagueTable.rows)
-    };
+      };
+      console.log('SO FAR TABLE: ', leagueTable.rows)
 
 
+    const allMatches = [];
+    const newMatch = {
+      team1: {},
+      team2: {},
+      score1: 0,
+      score2: 0
+    }
+    // Generate all matches possible in a group
+    const generatePossibleMatches = () => {
+      // console.log('DUPLICATE LEAGUE-TABLE:',duplicateTable)
+      if(leagueTable.rows.length % 2 == 0) {
+        
+        for(let m = 0; m < leagueTable.rows.length / 2; m++){
+          newMatch.team1 = leagueTable.rows[m]
+          for(let x = leagueTable.rows.length - 1; x < leagueTable.rows.length / 2; x--) {
+            newMatch.team2 = leagueTable.rows[x]
+          }
+          allMatches.push(newMatch)
+        }
+        console.log('ALL MATCHES GEN::: ', allMatches)
 
-    // Generate random pair
-    let duplicateTable = leagueTable.rows.slice();
-    const generateMatches = () => {
-      console.log(duplicateTable)
-      const newMatch = {
-        team1: '',
-        team2: '',
-        score1: 0,
-        score2: 0
+      }
+
+      // const secondHalf = leagueTable.rows.slice(leagueTable.rows.length/2, leagueTable.rows.length)
+      //   const firstHalf = [];
+      //   for(let y = 0; y < leagueTable.rows.length / 2; y++){
+      //     firstHalf.push(leagueTable.rows[y])
+      //   }
+
       }
       
-     
-      }
-      const matchButton = document.querySelector('#generate-button');
-      matchButton.addEventListener('click', generateMatches);
+
+      
+      const matchButton = document.querySelector('#generate-matches-button');
+      matchButton.addEventListener('click', generatePossibleMatches);
               
               
 })    
