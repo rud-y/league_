3e1999775894554b5d643768ba86ad7d8781bcc1
Rudolf Zachar <rudolf.zachar@reachplc.com>
2022-11-12 15:48:04 +0000

    Separate playGame

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
   // Main league table
   const leagueTable = {
@@ -130,8 +132,6 @@ document.addEventListener('DOMContentLoaded', () => {
         teamsPlayingThisWeek.push(match.team1);
         teamsPlayingThisWeek.push(match.team2);
         fixtures.push(match);
-      } else {
-        console.log('IN THIS WEEK ALREADY '+ match.team1+' and '+match.team2);
       }
     });
 
@@ -194,31 +194,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   matchButton.addEventListener('click', createMatchesUI);
-
-  //---START GAME--------
-  const playGame = (e) => {
-    let gameButton = e.target;
-    const matchElement = e.target.parentNode;
-    let score1 = matchElement.querySelector('.score-element1');
-    let score2 = matchElement.querySelector('.score-element2');
-    const team1 = matchElement.querySelector('.team-element1');
-    const team2 = matchElement.querySelector('.team-element2');
-
-    gameButton.innerHTML = '1.PERIOD IN PROGRESS';
-    setTimeout(() => {
-      score1.innerHTML = generateRandomScore(0, 3);
-      score2.innerHTML = generateRandomScore(0, 3);
-      console.log(team1.innerText + score1.innerHTML);
-      console.log(team2.innerText + score2.innerHTML);
-      gameButton.innerHTML = 'END OF 1.PERIOD';
-    }, 3000);
-
-  };
-
-  const generateRandomScore = (min, max) => {
-    const result = Math.random() * (max - min) + min;
-    return result.toFixed(0);
-  };
 
 });
 
---
+++ game/playGame.js
@@ -0,0 +1,26 @@
+//---START GAME--------
+const playGame = (e) => {
+    let gameButton = e.target;
+    const matchElement = e.target.parentNode;
+    let score1 = matchElement.querySelector('.score-element1');
+    let score2 = matchElement.querySelector('.score-element2');
+    const team1 = matchElement.querySelector('.team-element1');
+    const team2 = matchElement.querySelector('.team-element2');
+
+    gameButton.innerHTML = '1.PERIOD IN PROGRESS';
+    setTimeout(() => {
+      score1.innerHTML = generateRandomScore(0, 3);
+      score2.innerHTML = generateRandomScore(0, 3);
+      console.log(team1.innerText + score1.innerHTML);
+      console.log(team2.innerText + score2.innerHTML);
+      gameButton.innerHTML = 'END OF 1.PERIOD';
+    }, 3000);
+
+  };
+
+  const generateRandomScore = (min, max) => {
+    const result = Math.random() * (max - min) + min;
+    return result.toFixed(0);
+  };
+
+  module.exports = playGame;
